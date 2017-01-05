0b5cf1f92f57b63bd3565965ac0433f842672537
Joan Perez <jperezes@cisco.com>
2017-01-05 01:04:02 +0100

    fist dolores commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var port = process.env.PORT || 1337;
+var sparklite = require("sparklite");
+var botdomain = 'YOUR_SITE.azurewebsites.net';
+var sparkBot = new sparklite.SparkBot('MGVkNmMzYTQtYTU3MC00OTViLWI1N2ItMzc3NzMyNTkyNTgyNzRmMjE0NDItMjFi', port, botdomain);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+sparkBot.printHelloWorld();
+
+sparkBot.on('message', function (event)
+{
+    console.log('Incoming message: '+ JSON.stringify(event.message) + ' from: '+event.person.displayName );
+    var sentMessage = 'Hola ' + event.person.displayName;
+
+    sparkBot.sendMessage(event.roomId, sentMessage , function(){
+      console.log('Message sent from Bot!');
+    });
+    console.log(JSON.stringify(event));
+})
+
+sparkBot.on('rooms', function (event)
+{
+    console.log(JSON.stringify(event));
+})
+
+sparkBot.on('memberships', function (event)
+{
+    console.log(JSON.stringify(event));
+})
