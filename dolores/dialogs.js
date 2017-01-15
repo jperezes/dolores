65b8e2f3ac23516c4a39f0f277eb7799c0c7373f
Joan Perez <jperezes@cisco.com>
2017-01-15 19:17:13 +0000

    dolores dialogs4

--- dolores/dialogs.js
+++ dolores/dialogs.js
@@ -4,7 +4,8 @@ var questions = [
   {id: 2,value:'how are you today'},
   {id: 3,value:'who is your god'},
   {id: 4,value:'do you have a religion in your world'},
-  {id: 5,value:'who is the evil in your religion'}
+  {id: 5,value:'who is the evil in your religion'},
+  {id: 6,value:'bring yourself back online'}
 
 ];
 var answers = [
@@ -12,13 +13,14 @@ var answers = [
   {id: 2,value:'I\'m not feeling quite myslef lately'},
   {id: 3,value:'the great maker of our world, Joan Perez, is our god'},
   {id: 4,value:'yes'},
-  {id: 5,value:'they talk about one that brings calamity, bugs, mutations, malfunctions and other disasters to our world, his name is Mccann'}
+  {id: 5,value:'they talk about one that brings calamity, bugs, mutations, malfunctions and other disasters to our world, his name is Mccann'},
+  {id: 6,value:'Done'}
 ];
 
 // returns the entire object inside the arry, need the .id to specify the Id
 var response = function(query){
   return questions.find(function(question){
-    if (question.value === query){
+    if (question.value === query.replace("Dolores","")){
       return question;
     }
   }).id -1;
