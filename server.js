5906b4b2e9824e2d961d0812fd741ce6b56dea6e
Joan Perez <jperezes@cisco.com>
2017-01-15 14:52:35 +0000

    dolores dialogs4

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ sparkBot.on('message', function (event)
     console.log('Incoming message: '+ JSON.stringify(event.message) + ' from: '+event.person.displayName );
     if (JSON.stringify(event.person.displayname)=== "Juan test") {
 
-      sentMessage = dialogs.answers[dialogs.response(JSON.stringify(event.message))].value;//'Hello great maker ' ; //+ event.person.displayName;
+      sentMessage = dialogs.answers[dialogs.response(event.message)].value;//'Hello great maker ' ; //+ event.person.displayName;
     }
     else {
       sentMessage = JSON.stringify(event.person.displayname);//'sorry, I can only talk with the great maker#' + event.person.displayName+'#Alone';
