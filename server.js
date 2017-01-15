8a6a01eff7069b98342dbe736f1310258079c9de
Joan Perez <jperezes@cisco.com>
2017-01-15 18:45:17 +0000

    dolores dialogs4

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ sparkBot.on('message', function (event)
     }
     else {
       console.log('Incoming message: '+ JSON.stringify(event.message) + ' from: '+event.person.displayName + 'person name not parsed properly');
-      sentMessage = event.person.displayname + "  test";//'sorry, I can only talk with the great maker#' + event.person.displayName+'#Alone';
+      sentMessage = `${event.person.displayname}` + "  test";//'sorry, I can only talk with the great maker#' + event.person.displayName+'#Alone';
     }
 
 
