bac69861257569cda389732ea61038d2821aaa13
Joan Perez <jperezes@cisco.com>
2017-01-28 01:46:43 +0000

    changingport

--- dolores/routes/stadistics.js
+++ dolores/routes/stadistics.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3005;
+var port = 3011;
 var app = express();
 
 var botModule = function(){};
