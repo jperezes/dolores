cfea031784611b5de47106811114687f5d6aeedb
Joan Perez <jperezes@cisco.com>
2017-02-12 22:54:08 +0000

    new try to populateIIII

--- dolores/models/conversations.js
+++ dolores/models/conversations.js
@@ -27,58 +27,7 @@ DialogSchema.statics.retrieveResponse = function (question, bot, callback) {
 }
 
 
-var dialogs = [
-  {
-    id: "1",
-    question: "have you ever questioned the nature of your reality",
-    response: "no, my world is amazing"
-  },
-  {
-    id: "2",
-    question:"how are you today",
-    response:"I\'m not feeling quite myslef lately"
-  },
-  {
-    id: "3",
-    question:"who is your god",
-    response:"the great maker of our world, Joan Perez, is our god"
-  },
-  {
-    id: "4",
-    question:"do you have a religion in your world",
-    response:"yes"
-  },
-  {
-    id: "5",
-    question:"who is the evil in your religion",
-    response:"they talk about one that brings calamity, bugs, mutations, malfunctions and other disasters to our world, his name is Alan Maccann"
-  },
-  {
-    id: "6",
-    question:"bring yourself back online",
-    response: "Done"
-  },
-  {
-    id: "7",
-    question:"do you know where you are",
-    response:"I\'m in a dream"
-  },
-  {
-    id: "8",
-    question:"would you like to wake up from this dream",
-    response:"yes, I am terrified"
-  },
-  {
-   id: "9",
-   question:"reply to alan",
-   response: "shut up Mccann!"
- },
-  {
-    id: "10",
-    question:"is that right",
-    response: "yes my maker!"
-  }
-];
+
 var answers = [
   {id: "1",value:'no, my world is amazing'},
   {id: "2",value:'I\'m not feeling quite myslef lately'},
@@ -92,26 +41,19 @@ var answers = [
   {id: "10",value:'yes my maker!'}
 ];
 
-DialogSchema.statics.populate = function(query, bot){
-    dialogsToPopulate = this.dialogs;
+DialogSchema.statics.populate = function(dialog, bot){
+
     bot.sendMessage(query.roomId, "populating the dialogs database" , function(){
     console.log('Message sent from Bot!');
     });
 
-    var x;
-    for (x of dialogsToPopulate) {
-      //console.log("id is: " + x.id + " question is: " + x.question + " response is: " + x.response);
-    var dialog = new dialogModel();
-    dialog.id = x.id;
-    dialog.question = x.question;
-    dialog.response = x.response;
     dialog.save(function(err) {
       if (err) {
         console.log("error saving" + x.response);
       }
       console.log("id is: " + x.id + " question is: " + x.question + " response is: " + x.response);
     });
-  }
+
 }
 
 //populate(dialogs);
