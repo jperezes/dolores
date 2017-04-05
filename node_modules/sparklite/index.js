562d8fddc809a08577d46d7a32884f9960a002a8
Joan Perez <jperezes@cisco.com>
2017-04-05 23:08:01 +0100

    update packages

--- node_modules/sparklite/index.js
+++ node_modules/sparklite/index.js
@@ -210,6 +210,21 @@ SparkBot.prototype.sendMessage = function(roomId, txt, callback) {
     })
 }
 
+SparkBot.prototype.sendRichTextMessage = function(roomId, txt, callback) {
+    var optionsCloned = clone(this.getOptions());
+    var extendedOptions = extend(optionsCloned, {
+        path: "/v1/messages/"
+    });
+    extendedOptions['method'] = 'POST';
+    var messageData = {
+        'roomId': roomId,
+        'markdown': txt
+    }
+    SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
+        callback(result);
+    })
+}
+
 SparkBot.prototype.sendMessageWithFile = function(roomId, txt, filesUrl, callback) {
     var optionsCloned = clone(this.getOptions());
     var extendedOptions = extend(optionsCloned, {
@@ -221,7 +236,7 @@ SparkBot.prototype.sendMessageWithFile = function(roomId, txt, filesUrl, callbac
         'text': txt,
         'files': filesUrl
     }
-    
+
     SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
         callback(result);
     })
