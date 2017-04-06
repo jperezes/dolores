67261386bb6b27153beadf38038cabc39b743a62
Joan Perez <jperezes@cisco.com>
2017-04-06 23:03:46 +0100

    just a test

--- dolores/routes/stadistics.js
+++ dolores/routes/stadistics.js
@@ -74,7 +74,7 @@ botModule.prototype.listenForStadistics = function(bot,app){
           });
       })
       res.status(200).send('Splunk result successfully saved to the database');
-    }).catch(err => {res.send(err)});
+    }).catch(err => {res.status(204).send(err)});
 
   });
 }
